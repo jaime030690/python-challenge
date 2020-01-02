047912733e257983432cdbcff0ae27f58042d323
Jaime Echevarria <jaime030690@gmail.com>
2020-01-02 18:35:34 -0500

    Code for PyPoll homework.

---
+++ PyPoll/main.py
@@ -0,0 +1,42 @@
+#Import things
+import os
+import csv
+
+totalVotes = 0
+
+candidates = {
+    "Correy": [0, 0.0],
+    "Khan": [0, 0.0],
+    "Li": [0, 0.0],
+    "O'Tooley": [0, 0.0]
+}
+
+filename = os.path.join("Resources", "test_file.csv")
+
+with open(filename, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    #Skip header
+    next(csvfile)
+
+    for row in csvreader:
+
+        totalVotes += 1
+
+        if row[2] == "Correy":
+            candidates["Correy"][0] += 1
+
+        elif row[2] == "Khan":
+            candidates["Khan"][0] += 1
+
+        elif row[2] == "Li":
+            candidates["Li"][0] += 1
+        
+        elif row[2] == "O'Tooley":
+            candidates["O'Tooley"][0] += 1
+
+for key in candidates:
+    candidates[key][1] = 100 * candidates[key][0] / totalVotes
+
+print(f'Total votes: {totalVotes}')
+print(f'Results: {candidates}')
