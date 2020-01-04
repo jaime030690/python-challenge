91e6ffef0fa289087bf3a0bef647bff6633eaaa1
Jaime Echevarria <jaime030690@gmail.com>
2020-01-04 12:54:24 -0500

    Formatted results list.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,19 +3,18 @@ import os
 import csv
 
 totalVotes = 0
-
 candidates = {
-    "Correy": [0, 0],
     "Khan": [0, 0],
+    "Correy": [0, 0],
     "Li": [0, 0],
     "O'Tooley": [0, 0]
 }
-
-sortedCandidates = {}
 results = []
 
+#Get file to read
 filename = os.path.join("Resources", "election_data.csv")
 
+#Start reading file
 with open(filename, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
@@ -26,11 +25,11 @@ with open(filename, newline='') as csvfile:
 
         totalVotes += 1
 
-        if row[2] == "Correy":
-            candidates["Correy"][0] += 1
-
-        elif row[2] == "Khan":
+        if row[2] == "Khan":
             candidates["Khan"][0] += 1
+
+        elif row[2] == "Correy":
+            candidates["Correy"][0] += 1
 
         elif row[2] == "Li":
             candidates["Li"][0] += 1
@@ -38,17 +37,22 @@ with open(filename, newline='') as csvfile:
         elif row[2] == "O'Tooley":
             candidates["O'Tooley"][0] += 1
 
+#Calculate percentages
 for key in candidates:
     candidates[key][1] = 100 * candidates[key][0] / totalVotes
 
-sortedCandidates = sorted(candidates.items(), key = lambda kv:(kv[1], kv[0]), reverse = True)
-
+#Create list with output
 results = [
     f"Election Results",
     f"-------------------------",
     f"Total votes: {totalVotes}",
-    f"------------------------",
+    f"-------------------------",
+    f"Khan: {round(candidates["Khan"][1], 3)}% ({candidates["Khan"][0]})",
+    f"Correy: {round(candidates["Correy"][1], 3)}% ({candidates["Correy"][0]})",
+    f"Li: {round(candidates["Li"][1], 3)}% ({candidates["Li"][0]})",
+    f"O'Tooley: {round(candidates["O'Tooley"][1], 3)}% ({candidates["O'Tooley"][0]})"
 ]
 
-print(f'Total votes: {totalVotes}')
-print(f'Results: {sortedCandidates}')
+#Print results
+for item in results:
+    print(results[item])
