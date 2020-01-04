a57babfe7ca5a7cc7f005f31b5fba9e11f60e748
Jaime Echevarria <jaime030690@gmail.com>
2020-01-04 13:29:16 -0500

    Removed dictionaries.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,14 +2,16 @@
 import os
 import csv
 
-totalVotes = 0
-candidates = {
-    "Khan": [0, 0],
-    "Correy": [0, 0],
-    "Li": [0, 0],
-    "O'Tooley": [0, 0]
-}
+candidate_khan = 0
+candidate_correy = 0
+candidate_li = 0
+candidate_otooley = 0
+
+total_votes = 0
+
 results = []
+winner = 0
+winnerName = ''
 
 #Get file to read
 filename = os.path.join("Resources", "election_data.csv")
@@ -23,36 +25,36 @@ with open(filename, newline='') as csvfile:
 
     for row in csvreader:
 
-        totalVotes += 1
+        total_votes += 1
 
         if row[2] == "Khan":
-            candidates["Khan"][0] += 1
+            candidate_khan += 1
 
         elif row[2] == "Correy":
-            candidates["Correy"][0] += 1
+            candidate_correy += 1
 
         elif row[2] == "Li":
-            candidates["Li"][0] += 1
+            candidate_li += 1
         
         elif row[2] == "O'Tooley":
-            candidates["O'Tooley"][0] += 1
+            candidate_otooley += 1
 
-#Calculate percentages
-for key in candidates:
-    candidates[key][1] = 100 * candidates[key][0] / totalVotes
+
 
 #Create list with output
 results = [
     f"Election Results",
     f"-------------------------",
-    f"Total votes: {totalVotes}",
+    f"Total votes: {total_votes}",
     f"-------------------------",
-    f"Khan: {round(candidates["Khan"][1], 3)}% ({candidates["Khan"][0]})",
-    f"Correy: {round(candidates["Correy"][1], 3)}% ({candidates["Correy"][0]})",
-    f"Li: {round(candidates["Li"][1], 3)}% ({candidates["Li"][0]})",
-    f"O'Tooley: {round(candidates["O'Tooley"][1], 3)}% ({candidates["O'Tooley"][0]})"
+    f"Khan: {round((100 * candidate_khan / total_votes), 3)}% ({candidate_khan})",
+    f"Correy: {round((100 * candidate_correy / total_votes), 3)}% ({candidate_correy})",
+    f"Li: {round((100 * candidate_li / total_votes), 3)}% ({candidate_li})",
+    f"O'Tooley: {round((100 * candidate_otooley / total_votes), 3)}% ({candidate_otooley})",
+    f"-------------------------",
+    f"Winner: "
 ]
 
-#Print results
-for item in results:
-    print(results[item])
+#Print results to terminal
+for line in results:
+    print(line)
